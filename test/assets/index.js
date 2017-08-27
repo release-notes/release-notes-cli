e3ada1514c1a713095a3b259f011e81424b81640
IjzerenHein <hrutjes@gmail.com>
2017-08-27 16:31:55 +0200

    Added unit tests for ‘convert’ and ‘validate’ commands
    + added:
    - .editorconfig which specifies that editors should use spaces for this project
    -
    
    # Conflicts:
    #	package.json

---
+++ test/assets/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const releaseNotes1 = require('./release-notes1.json');
+
+module.exports = {
+  releaseNotes1: {
+    yamlPath: './test/assets/release-notes1.yml',
+    jsonPath: './test/assets/release-notes1.json',
+    jsonData: releaseNotes1,
+  },
+};
---
+++ test/convert.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { run, assert } = require('./util');
+const { releaseNotes1 } = require('./assets');
+
+describe('convert', () => {
+  describe('subcommands', () => {
+    it('should fail when type is omitted', () => {
+      return assert.isRejected(run('convert'));
+    });
+    it('should support type alias -t', () => {
+      return assert.isFulfilled(run('convert -t json'));
+    });
+    it('should fail when non existent file is specified', () => {
+      return assert.isRejected(run('convert ./test/assets/release-notes-invalid.yml -t json'));
+    });
+    it('should fail when invalid file is specified', () => {
+      return assert.isRejected(run('convert ./test/assets/release-notes1.json -t json'));
+    });
+    it('should fail when unknown type is specified', () => {
+      return assert.isRejected(run('convert --type josn'));
+    });
+    it('should fail when wrong type sub-command is specified', () => {
+      return assert.isRejected(run('convert --typo json'));
+    });
+  });
+
+  describe('json', () => {
+    it('should convert to JSON and write to stdout', () => {
+      const json = run(`convert ${releaseNotes1.yamlPath} -t json`).then(text => JSON.parse(text));
+      return assert.eventually.deepEqual(json, releaseNotes1.jsonData);
+    });
+  });
+});
