d3848d5626818bb2f834c0f6eb2dfec476027ea0
Alrik Zachert <az@kreatiefer.de>
2017-09-14 14:00:45 +0200

    Allow for optional args instead of calling command with `undefined` as parameter.

--- test/util.js
+++ test/util.js
@@ -10,7 +10,7 @@ const { assert } = chai;
 
 function run(args) {
   return new Promise((resolve, reject) => {
-    childProcess.exec(`./bin/release-notes.js ${args}`, {
+    childProcess.exec(`./bin/release-notes.js ${args || ''}`, {
       encoding: 'utf8',
     }, (error, stdout) => {
       if (error) return reject(error);
