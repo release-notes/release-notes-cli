897d9b173c33b1fc4f1a8dd8794772be53a629c3
Alrik Zachert <az@kreatiefer.de>
2017-09-14 14:03:12 +0200

    Test for command failure when called without any argument.

--- test/base.spec.js
+++ test/base.spec.js
@@ -1,10 +1,13 @@
 'use strict';
 
-
 const { run, assert } = require('./util');
 
 describe('commands', () => {
-  it.skip('should fail when invalid command is specified', () => {
+  it('should fail when no command is specified', () => {
+    return assert.isRejected(run());
+  });
+
+  it('should fail when invalid command is specified', () => {
     return assert.isRejected(run('conv1'));
   });
 });
