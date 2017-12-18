ec13c82be0c680f356c27541fd0c37e339304130
Alrik Zachert <az@kreatiefer.de>
2017-12-18 22:23:19 +0100

    Add markdown version of our release notes test asset. (#14)

--- test/assets/index.js
+++ test/assets/index.js
@@ -6,6 +6,7 @@ module.exports = {
   releaseNotes1: {
     yamlPath: './test/assets/release-notes1.yml',
     jsonPath: './test/assets/release-notes1.json',
+    mdPath: './test/assets/release-notes1.md',
     jsonData: releaseNotes1,
   },
 };
